92cf568fc5c2965a6cc0cd81b5aa038c2bc26653
pcorange <471378935@qq.com>
2017-08-27 19:35:57 +0800

    first commit to master (local repository)

---
+++ WindowsFormsApp2_最终版/WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(378, 214);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(71, 25);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "START";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button2.Location = new System.Drawing.Point(378, 245);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(71, 25);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "STOP";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(258, 271);
+            this.textBox1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(296, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(155, 21);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "已处理的FAIL数量：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(442, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 21);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "label2";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(292, 203);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(72, 80);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(300, 87);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(91, 33);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "打开文件位置";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(301, 134);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 21);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "请选择文件";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(518, 295);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ WindowsFormsApp2_最终版/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using System.IO;
+
+namespace WindowsFormsApp2
+{   
+    enum ProcessResult
+    {
+        SUCCESS = 0,
+        FAIL = 1
+    }
+
+    public partial class Form1 : Form
+    {
+        int count = 0;
+        bool status;
+        string[] path;
+        bool path_isset;
+        object[] param = new object[2];
+        SynchronizationContext work_SyncContext = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+            label2.Text = count.ToString();
+            status = false;
+            path_isset = false;
+            work_SyncContext = SynchronizationContext.Current;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (path_isset == true)
+            {
+                if (status == false)
+                {
+                    status = true;
+                    ThreadPool.QueueUserWorkItem(Work, null);
+                }
+            }
+            else
+            {
+                label3.Text = "失败！未选择文件";
+            }
+            
+        }
+
+        public void Work(object obj)
+        {
+            while (status)
+            {
+                foreach (string p in path)
+                {
+                    try
+                    {
+                        FileStream file = new FileStream(p, FileMode.Open);
+                        StreamReader sr = new StreamReader(file);
+                        string content = sr.ReadToEnd();
+                        StringBuilder newcontent = new StringBuilder();
+                        string[] sArray = content.Split('');
+                        for (int i = 0; i < sArray.Length; i++)
+                        {
+                            if (sArray[i].IndexOf("RESULT : PASS") >= 0)
+                            {
+                                newcontent.Append(sArray[i]);
+                                newcontent.Append('');
+                            }
+                            else if (sArray[i].IndexOf("RESULT : FAIL") >= 0)
+                            {
+                                count++;
+                            }
+                        }
+                        sr.Close();
+                        file.Close();
+
+                        FileStream file2 = new FileStream(p, FileMode.Create);
+                        StreamWriter sw = new StreamWriter(file2);
+                        sw.Write(newcontent);
+                        sw.Close();
+                        file2.Close();
+
+                        param[0] = ProcessResult.SUCCESS;
+                        param[1] = Path.GetFileNameWithoutExtension(p);
+                        work_SyncContext.Post(UpdateMessage, param);
+
+                        GC.Collect();
+                    }
+                    catch (Exception e)
+                    {
+                        param[0] = ProcessResult.FAIL;
+                        param[1] = Path.GetFileNameWithoutExtension(p);
+                        work_SyncContext.Post(UpdateMessage, param);
+
+                        DateTime dt = DateTime.Now;
+                        string[] stime = dt.ToShortDateString().Split('/');
+                        StringBuilder time = new StringBuilder();
+                        foreach (string t in stime)
+                        {
+                            time.Append(t);
+                        }
+                        if (!System.IO.Directory.Exists(@"C:\log_fatred"))
+                        {
+                            System.IO.Directory.CreateDirectory(@"C:\log_fatred");
+                        }
+                        string address = @"C:\log_fatred\" + time + "_dialog.txt";
+                        FileStream file3 = new FileStream(address, FileMode.Append, FileAccess.Write);
+                        StreamWriter sw_log = new StreamWriter(file3);
+                        sw_log.WriteLine("********************************************************************************");
+                        sw_log.WriteLine(dt.ToLocalTime().ToString());
+                        sw_log.WriteLine(e.ToString());
+                        sw_log.WriteLine("********************************************************************************");
+                        sw_log.WriteLine("\n");
+                        sw_log.Close();
+                        file3.Close();
+
+                        GC.Collect();
+                    }
+                    /*
+                    finally
+                    {
+                        Thread.CurrentThread.Join(3000);
+                    }
+                    */
+                }
+                Thread.CurrentThread.Join(3000);
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            status = false;
+    
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog filename = new OpenFileDialog();
+            filename.InitialDirectory = Application.StartupPath;
+            filename.Filter = "csv文件|*.csv";
+            filename.RestoreDirectory = true;
+            filename.Multiselect = true;
+            if (filename.ShowDialog() == DialogResult.OK)
+            {
+                path = filename.FileNames;
+                path_isset = true;
+                label3.Text = "文件设置成功！";
+                /*
+                foreach (string p in path)
+                {
+                    textBox1.AppendText(p+"\n");
+                }
+                */
+            }
+        }
+
+        private void UpdateMessage(object obj)
+        {
+            object[] param = obj as object[];
+            ProcessResult result = (ProcessResult)param[0];
+            string filename = param[1] as string;
+            string showtext;
+
+            if (result == ProcessResult.SUCCESS)
+            {
+                showtext = string.Format("处理{0}：SUCCESS\n", filename);
+                textBox1.AppendText(showtext);
+                label2.Text = count.ToString();
+            }
+            if (result == ProcessResult.FAIL)
+            {
+                showtext = string.Format("处理{0}：FAIL\n", filename);
+                textBox1.AppendText(showtext);
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
